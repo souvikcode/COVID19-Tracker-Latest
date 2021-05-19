e31dc28feb0664d1238aaeffa6bd8fe9de518f07
Souvik Mondal <53980212+souvikcode@users.noreply.github.com>
2021-05-19 18:52:03 +0530

    Update Client.kt

--- app/src/main/java/com/example/covid19_tracker/Client.kt
+++ app/src/main/java/com/example/covid19_tracker/Client.kt
@@ -4,11 +4,11 @@ import okhttp3.OkHttpClient
 import okhttp3.Request
 
 object Client {
-    private val okHttpClient = OkHttpClient()
+    private val okHttpClient = OkHttpClient()  //OkHTTP Client formed, this we use for Networking
 
-    private val request = Request.Builder()
+    private val request = Request.Builder()  // Used for Requesting Information
         .url("https://api.covid19india.org/data.json")
         .build()
 
-    val api = okHttpClient.newCall(request)
+    val api = okHttpClient.newCall(request)  // This tells that we have to call "request" from okHttpClient
   }
